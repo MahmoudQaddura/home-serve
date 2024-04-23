b8cf1e62c45a6a3fbb4a931d074af9cf21a20f9f
Mahmoud Qaddura <kmahmoud1898@gmail.com>
2024-04-23 12:17:25 +0300

    updated script to be more robust

--- front/ip.bat
+++ front/ip.bat
@@ -9,26 +9,37 @@ for /f "tokens=2 delims=:" %%a in ('ipconfig ^| findstr /i "IPv4 Address"') do (
 
 REM Set .env.local file path
 set "file=.env.local"
-set "search=VITE_BACK_IP=(ip here)"
-set "replace=VITE_BACK_IP=!ip!"
+set "search=VITE_BACK_IP="
 
 REM Check if .env.local file exists
 if not exist "%file%" (
     REM Create .env.local file and add IP
-    echo %replace%> "%file%"
+    echo %search%!ip!> "%file%"
     echo .env.local created successfully.
 ) else (
-    REM Perform the replacement
-    (for /f "usebackq delims=" %%i in ("%file%") do (
-        set "line=%%i"
-        if "!line!" equ "%search%" (
-            echo %replace%
-        ) else (
-            echo %%i
-        )
-    )) > "%file%.tmp"
+    REM Check if .env.local file contains VITE_BACK_IP=
+    findstr /C:"%search%" "%file%" >nul
+    if errorlevel 1 (
+        REM Append IP to .env.local file
+        echo %search%!ip!>> "%file%"
+        echo VITE_BACK_IP added to .env.local
+    ) else (
+        REM Perform the replacement
+        (for /f "usebackq tokens=*" %%i in ("%file%") do (
+            set "line=%%i"
+  
+            echo !line! | findstr /C:"%search%" > nul
+            if not errorlevel 1 (
 
-    REM Replace the original file with the updated one
-    move /y "%file%.tmp" "%file%" >nul
-    echo .env.local updated successfully.
+                echo %search%!ip!
+            ) else (
+ 
+                echo %%i
+            )
+        )) > "%file%.tmp"
+
+        REM Replace the original file with the updated one
+        move /y "%file%.tmp" "%file%" >nul
+        echo .env.local updated successfully.
+    )
 )
